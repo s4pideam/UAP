b47dfd31a2e580e77a9b0973eff5518e1a93e2a3
s4pideam <50202418+s4pideam@users.noreply.github.com>
2023-11-09 06:02:22 +0100

    Update AbstractMachine.java

--- src/main/java/de/unitrier/st/uap/w23/tram/AbstractMachine.java
+++ src/main/java/de/unitrier/st/uap/w23/tram/AbstractMachine.java
@@ -2,6 +2,7 @@ package de.unitrier.st.uap.w23.tram;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
 
 import java.util.Arrays;
 import java.util.Scanner;
